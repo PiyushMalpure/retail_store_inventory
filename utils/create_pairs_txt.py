b341c363e742f4c5b30cb286511af29252d7d597
Piyush Malpure <malpurepiyush07@gmail.com>
2024-02-25 21:54:04 -0600

    code to create input pairs for superGlue

---
+++ utils/create_pairs_txt.py
@@ -0,0 +1,24 @@
+import os
+
+def create_pairs_txt(query_folder_path, reference_folder_path, pairs_txt_path):
+    """
+    Create a pairs.txt file for SuperGlue
+    Args:
+        query_folder_path: str - path to the query folder
+        reference_folder_path: str - path to the reference folder
+        pairs_txt_path: str - path to save the pairs.txt file
+    """
+    query_images = os.listdir(query_folder_path)
+    reference_images = os.listdir(reference_folder_path)
+    
+    with open(pairs_txt_path, 'w') as f:
+        for query_image in query_images:
+            for reference_image in reference_images:
+                f.write(f"{query_image} {reference_image}\n")
+
+if __name__ == '__main__':
+    query_folder_path = '../results/predict/crops/retail'
+    reference_folder_path = '../data/images_inventory'
+    pairs_txt_path = '../data/pairs.txt'
+    create_pairs_txt(query_folder_path, reference_folder_path, pairs_txt_path)
+    
